0d906adf97e2cb34dbab49cd2524c7a3bef326dc
vikash <vikashkumarsharma@navneettoptech.com>
2026-08-16 15:57:44 +0530

    fixed all formating error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const name = 'vikash';
+const age = 25;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/json',
