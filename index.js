e1663fdb15749e12a6cae78903583913b0c4aa94
vikash <vikashkumarsharma@navneettoptech.com>
2026-08-16 11:56:46 +0530

    add new comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {http} from 'http';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World GIT HUB ACTION\n');
 });
 
 server.listen(3000, () => {
