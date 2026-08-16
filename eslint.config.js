9fb8029d980b9589c771813d7ffc965f7ceeb3fe
vikash <vikashkumarsharma@navneettoptech.com>
2026-08-16 15:32:25 +0530

    fix all format

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+];
