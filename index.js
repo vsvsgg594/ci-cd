85e5467c3d876bd3b94936e48ac49a6f52a79166
vikash <vikashkumarsharma@navneettoptech.com>
2026-08-16 15:42:06 +0530

    fix all format

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
-const name="vikash";
+const name = 'vikash';
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/json',
